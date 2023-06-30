038d38f6e8928b11593305effa8c37d2f39567bf
MuneneCalvin <munenecalvn@gmail.com>
2023-06-30 14:18:46 +0300

    Fixed 0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
-"""
-Prime Game
-"""
+""" Module for solving prime game question """
 
 def isWinner(x, nums):
-    """
-    Given a set of consecutive integers starting from 1 up to and including n,
-    hey take turns choosing a prime number from the set and removing that number
-    and its multiples from the set. The player that cannot make a move loses the game.
-    """
-    def is_prime(n):
-        if n <= 1:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    def get_prime_count(n):
-        count = 0
-        for i in range(2, n + 1):
-            if is_prime(i):
-                count += 1
-        return count
-
-    maria = 0
-    ben = 0
-
-    for num in nums:
-        prime_count = get_prime_count(num)
-        if prime_count % 2 == 0:
-            ben += 1
-        else:
-            maria += 1
-
-    if maria > ben:
-        return "Maria"
-    elif ben > maria:
-        return "Ben"
-    else:
+    """function that checks for the winner"""
+    if not nums or x < 1:
         return None
+    max_num = max(nums)
 
-
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
